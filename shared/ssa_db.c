e834b7e922534b95215957e0e20098ba55991b23
Ilya Nelkenbaum <ilyan@dev.mellanox.co.il>
2013-11-11 16:40:45 +0200

    ssa_db.[c h]: API update
    A new structure was added "struct ssa_db" uniting separate
    database tables and their definitions into a single structure.
    
    Create / Init / Destroy methods were added as well and implemented
    in shared/ssa_db.c.
    
    The following steps have to be taken when creating new DB:
    [1] "struct db_table_def def_tbl_arr[]" containing all DB tables defs.
    [2] "struct db_dataset dataset_tbl_arr[]" containing all datasets
        (1 separate array for data tables datasets and another one for
         field tables datasets).
    [3] "struct db_field_def field_tbl_arr[]" containing all data tables
        field definitions.
    [4] "ssa_db_create()" and "ssa_db_init()" methods have to be
        sequantially called.
    
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>
    Acked-by: Sean Hefty <sean.hefty@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- shared/ssa_db.c
+++ shared/ssa_db.c
@@ -136,3 +136,206 @@ void ssa_db_field_def_insert(struct db_field_def * p_tbl,
 	p_dataset->set_count = htonll(ntohll(p_dataset->set_count) + 1);
 	p_dataset->set_size = htonll(ntohll(p_dataset->set_size) + sizeof(*p_tbl));
 }
+
+/** =========================================================================
+ */
+struct ssa_db *ssa_db_create(uint64_t * p_num_recs_arr,
+			     size_t * p_data_recs_size_arr,
+			     uint64_t * p_num_field_recs_arr,
+			     uint64_t tbl_cnt)
+{
+	struct ssa_db *p_db;
+	int i, k;
+
+	p_db = (struct ssa_db *) calloc(1, sizeof(*p_db));
+	if (!p_db)
+		goto err1;
+
+	/* number of data & field tables = tbl_cnt * 2 */
+	p_db->p_def_tbl = (struct db_table_def *)
+				calloc(tbl_cnt * 2, sizeof(*p_db->p_def_tbl));
+	if (!p_db->p_def_tbl)
+		goto err2;
+
+	p_db->p_db_tables = (struct db_dataset *)
+				calloc(tbl_cnt, sizeof(*p_db->p_db_tables));
+	if (!p_db->p_db_tables)
+		goto err3;
+
+	p_db->p_db_field_tables = (struct db_dataset *)
+				calloc(tbl_cnt, sizeof(*p_db->p_db_field_tables));
+	if (!p_db->p_db_field_tables)
+		goto err4;
+
+	p_db->pp_tables = (void **) calloc(tbl_cnt, sizeof(*p_db->pp_tables));
+	if (!p_db->pp_tables)
+		goto err5;
+
+	for (i = 0; i < tbl_cnt; i++) {
+		p_db->pp_tables[i] =
+			(void *) malloc(p_data_recs_size_arr[i] * p_num_recs_arr[i]);
+		if (!p_db->pp_tables[i]) {
+			for (k = i - 1; k >= 0; k--)
+				free(p_db->pp_tables[k]);
+			goto err6;
+		}
+	}
+
+	p_db->pp_field_tables =
+		(struct db_field_def **) calloc(tbl_cnt, sizeof(*p_db->pp_field_tables));
+	if (!p_db->pp_field_tables)
+		goto err7;
+
+	for (i = 0; i < tbl_cnt; i++) {
+		if (p_num_field_recs_arr[i] == DB_VARIABLE_SIZE)
+			continue;
+
+		p_db->pp_field_tables[i] =
+			(struct db_field_def *) calloc(p_num_field_recs_arr[i],
+						       sizeof(**(p_db->pp_field_tables)));
+		if (!p_db->pp_field_tables[i]) {
+			for (k = i - 1; k >= 0; k--)
+				free(p_db->pp_field_tables[k]);
+			goto err8;
+		}
+	}
+
+	p_db->data_tbl_cnt = tbl_cnt;
+
+	return p_db;
+err8:
+	free(p_db->pp_field_tables);
+err7:
+	for (k = tbl_cnt - 1; k >= 0; k--)
+		free(p_db->pp_tables[k]);
+err6:
+	free(p_db->pp_tables);
+err5:
+	free(p_db->p_db_field_tables);
+err4:
+	free(p_db->p_db_tables);
+err3:
+	free(p_db->p_def_tbl);
+err2:
+	free(p_db);
+err1:
+	return NULL;
+}
+
+/** =========================================================================
+ */
+void ssa_db_init(struct ssa_db * p_ssa_db, char * name, uint8_t db_id,
+		 const struct db_table_def *def_tbl,
+		 const struct db_dataset *dataset_tbl,
+		 const struct db_dataset *field_dataset_tbl,
+		 const struct db_field_def *field_tbl)
+{
+	const struct db_table_def *p_tbl_def = NULL;
+	const struct db_dataset *p_dataset = NULL;
+	const struct db_field_def *p_field_def = NULL;
+
+	if (!p_ssa_db)
+		return;
+
+	/*
+	 * Database definition initialization
+	 */
+	ssa_db_def_init(&p_ssa_db->db_def, DB_DEF_VERSION,
+			sizeof(p_ssa_db->db_def), db_id,
+			0, 0, name, sizeof(*p_ssa_db->p_def_tbl));
+
+	/*
+	 * Definition tables dataset initialization
+	 */
+	ssa_db_dataset_init(&p_ssa_db->db_table_def, DB_DS_VERSION,
+			    sizeof(p_ssa_db->db_table_def),
+			    DBT_ACCESS_NET_ORDER, db_id, DBT_DEF_DS_ID,
+			    0, 0 /* epoch */, 0 /* set_size */,
+			    0 /* set_offset */, 0 /* set_count */);
+
+	/* adding table definitions */
+	for (p_tbl_def = def_tbl; p_tbl_def->version; p_tbl_def++)
+		ssa_db_table_def_insert(p_ssa_db->p_def_tbl,
+					&p_ssa_db->db_table_def,
+					p_tbl_def->version, p_tbl_def->size,
+					p_tbl_def->type, p_tbl_def->access,
+					p_tbl_def->id.db, p_tbl_def->id.table,
+					p_tbl_def->id.field, p_tbl_def->name,
+					ntohl(p_tbl_def->record_size),
+					ntohl(p_tbl_def->ref_table_id));
+
+	/* data tables datasets initialization */
+	for (p_dataset = dataset_tbl; p_dataset->version; p_dataset++)
+		ssa_db_dataset_init(&p_ssa_db->p_db_tables[p_dataset->id.table],
+				    p_dataset->version, p_dataset->size,
+				    p_dataset->access, p_dataset->id.db,
+				    p_dataset->id.table, p_dataset->id.field,
+				    p_dataset->epoch, p_dataset->set_size,
+				    p_dataset->set_offset,
+				    p_dataset->set_count);
+
+	/* field tables datasets initialization */
+	for (p_dataset = dataset_tbl; p_dataset->version; p_dataset++)
+		ssa_db_dataset_init(&p_ssa_db->p_db_field_tables[p_dataset->id.table],
+				    p_dataset->version, p_dataset->size,
+				    p_dataset->access, p_dataset->id.db,
+				    p_dataset->id.table, p_dataset->id.field,
+				    p_dataset->epoch, p_dataset->set_size,
+				    p_dataset->set_offset,
+				    p_dataset->set_count);
+
+	/* field tables initialization */
+	for (p_tbl_def = def_tbl; p_tbl_def->version; p_tbl_def++) {
+		uint8_t tbl_id = ntohl(p_tbl_def->ref_table_id);
+		if (p_tbl_def->type != DBT_TYPE_DEF)
+			continue;
+		for (p_field_def = field_tbl; p_field_def->version; p_field_def++) {
+                        if (p_field_def->id.table == p_tbl_def->id.table) {
+                                ssa_db_field_def_insert(p_ssa_db->pp_field_tables[tbl_id],
+                                                        &p_ssa_db->p_db_field_tables[tbl_id],
+                                                        p_field_def->version, p_field_def->type,
+                                                        p_field_def->id.db, p_field_def->id.table,
+                                                        p_field_def->id.field, p_field_def->name,
+                                                        ntohl(p_field_def->field_size),
+                                                        ntohl(p_field_def->field_offset));
+			}
+                }
+        }
+}
+
+/** =========================================================================
+ */
+void ssa_db_destroy(struct ssa_db * p_ssa_db)
+{
+	uint64_t i, tbl_cnt;
+
+	if (!p_ssa_db)
+		return;
+
+	tbl_cnt = p_ssa_db->data_tbl_cnt;
+
+	for (i = tbl_cnt - 1; i >= 0; i--) {
+		if (!p_ssa_db->pp_field_tables[i])
+			continue;
+		free(p_ssa_db->pp_field_tables[i]);
+		p_ssa_db->pp_field_tables[i] = NULL;
+	}
+	free(p_ssa_db->pp_field_tables);
+
+	for (i = tbl_cnt - 1; i >= 0; i--) {
+		free(p_ssa_db->pp_tables[i]);
+		p_ssa_db->pp_tables[i] = NULL;
+	}
+	free(p_ssa_db->pp_tables);
+	p_ssa_db->pp_tables = NULL;
+
+	free(p_ssa_db->p_db_field_tables);
+	free(p_ssa_db->p_db_tables);
+	p_ssa_db->p_db_tables = NULL;
+
+	free(p_ssa_db->p_def_tbl);
+	p_ssa_db->p_def_tbl = NULL;
+
+	free(p_ssa_db);
+	p_ssa_db = NULL;
+}
